2594da39154343ea6f2198f2a0f313016a911412
Dmitry Shevchuk <dmitriy_shevchuk_01@ukr.net>
2021-04-21 20:36:22 +0300

    add server.js and procfile

---
+++ server.js
@@ -0,0 +1,14 @@
+const { static } = require('express');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('public/db.json');
+const middlewares = jsonServer.defaults('/build');
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log('JSON Server is running');
+});
