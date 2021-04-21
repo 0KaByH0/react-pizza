8d11afa75ff81e9247528fb442cfd6b552bacbc5
Dmitry Shevchuk <dmitriy_shevchuk_01@ukr.net>
2021-04-21 20:46:07 +0300

    fixed server bugs 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { static } = require('express');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
-const middlewares = jsonServer.defaults('./build');
+const middlewares = jsonServer.defaults({ static: './build' });
 
 server.use(middlewares);
 server.use(router);
