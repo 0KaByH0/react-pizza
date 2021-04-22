f725a0725d0dd9eeee3d320b5e185afae2a65c37
Dmitry Shevchuk <dmitriy_shevchuk_01@ukr.net>
2021-04-22 11:14:00 +0300

    refactored axios

--- src/redux/action/pizzas.js
+++ src/redux/action/pizzas.js
@@ -9,16 +9,16 @@ export const fetchPizzas = (category, type, order) => (dispatch) => {
   dispatch(setLoaded(false));
 
   // for develop
-  axios(
-    `http://localhost:3001/pizzas${
-      category === 0 ? `?` : `?category=${category}&`
-    }_sort=${type}&_order=${order}`,
-  ).then(({ data }) => dispatch(setPizzas(data)));
+  // axios(
+  //   `http://localhost:3001/pizzas${
+  //     category === 0 ? `?` : `?category=${category}&`
+  //   }_sort=${type}&_order=${order}`,
+  // ).then(({ data }) => dispatch(setPizzas(data)));
 
   // for production
-  // axios
-  //   .get(`/pizzas${category === 0 ? `?` : `?category=${category}&`}_sort=${type}&_order=${order}`)
-  //   .then(({ data }) => dispatch(setPizzas(data)));
+  axios
+    .get(`/pizzas${category === 0 ? `?` : `?category=${category}&`}_sort=${type}&_order=${order}`)
+    .then(({ data }) => dispatch(setPizzas(data)));
 };
 
 export const setPizzas = (items) => ({
